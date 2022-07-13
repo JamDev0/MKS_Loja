0d86a8cc519269821d3b9a96e88bf246f7a8758c
JAMDev <jamdeveloper0@gmail.com>
2022-07-12 21:12:36 -0300

    Saga que trata o pedido de requisição e o armazenamento do retorno dos produtos

---
+++ src/redux/saga/handlers/products.ts
@@ -0,0 +1,28 @@
+import { PayloadAction } from "@reduxjs/toolkit";
+
+import { call, put } from "redux-saga/effects";
+
+import { productInterface, setProducts } from "../../slices/productsSlice";
+
+import { fetchProducts } from "../requests/products";
+
+interface getProductsPayload {
+    id: number;
+    name: string;
+    brand: string;
+    description: string;
+    price: number;
+}
+
+export function* handleGetProducts(action: PayloadAction<getProductsPayload>) {
+    try {
+        const response = yield call(fetchProducts, action.payload)
+
+        const { data } = response;
+
+        yield put(setProducts(data.products as productInterface[]));
+    }
+    catch (error) {
+        yield console.error(error)
+    }
+}
