2b0d5997cbcc60492bc99c0b630420f94b17e7d5
JAMDev <jamdeveloper0@gmail.com>
2022-07-12 21:09:28 -0300

    Criação do tema default do app

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,28 @@
+export const defaultTheme = {
+    'colors': {
+        'brand': {
+            'blue': {
+                '500': '#0F52BA'
+            },
+
+            'black': {
+                '500': '#000000'
+            },
+
+            'gray': {
+                '500': '#373737'
+            }
+        },
+
+        'surfaces': {
+            'background': '#F9F9F9',
+            'footer': '#EEEEEE',
+            'elements': '#FFFFFF',
+            'text': '#2C2C2C'
+        }
+    },
+
+    'screens': {
+        'mobile': '780px'
+    }
+}
